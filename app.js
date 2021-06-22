146297e4e73071d9c222e7ae3c6c8a9ba2dcd47b
yayxs <leaderywl@163.com>
2021-06-22 18:45:46 +0800

    feat(add async): add async

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const serveIndex = require("serve-index");
+app.use(serveIndex("./src")); // 进行浏览目录
+// 利用express.static中间件来托管静态资源。
+app.use(express.static(path.join(__dirname, "src")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+app.listen(2021, () => {
+  console.log(`http://localhost:2021`);
+});
